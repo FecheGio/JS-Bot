5f8132cdaf02d4e9564a76370e511f8f824c0ff9
gillchristian <gillchristiang@gmail.com>
2016-12-13 15:21:10 -0300

    :hammer: Modularize code.

--- app.js
+++ app.js
@@ -1,45 +1,45 @@
+require('dotenv').config()
 
-// npm install twitter
-// API de twitter requerida
+var twitter = require('twitter')
+var tweetLogger = require('./logger')
+var userAuth = require('./config').userAuth
 
-var TwitterPackage = require('twitter');
-
-/*
-      Useful
-     Name: tweet.author.name
-     Screen-name (@tweet.author.screen_name): tweet.author.screen_name
-     Date UTC: tweet.created_at
-     Tweet: tweet.text
-     Retweeted: tweet.retweeted
-     Favourited: tweet.favorited
+/**
+ * Useful
+ *
+ * Name: tweet.author.name
+ * Screen-name (@tweet.author.screen_name): tweet.author.screen_name
+ * Date (UTC): tweet.created_at
+ * Tweet: tweet.text
+ * Retweeted: tweet.retweeted
+ * Favourited: tweet.favorited
 */
 
-var userAuth = {
-  consumer_key: 'put yours',
-  consumer_secret: 'put yours',
-  access_token_key: 'put yours',
-  access_token_secret: 'put yours'
-}
 
-var Twitter = new TwitterPackage(userAuth);
+var bot = new twitter(userAuth)
 
 // Stream tweets
+// mostrará solo los tweets con la propiedad de 'track:'
+bot.stream('statuses/filter', {track: '#sarasabot'}, function(stream) {
+  stream.on('data', function(tweet) {g
 
-Twitter.stream('statuses/filter', {track: '#sarasabot'}, function(stream) { //mostrará solo los tweets con la propiedad de "track:"
-  stream.on('data', function(tweet) {
+    // muestra en consola el tweet que contenga dicha propiedad
+    tweetLogger.in(tweet.text)
 
-    console.log(tweet.text);              //muestra en consola el tweet que contenga dicha propiedad
-    console.log('-------------');
+    // respuesta genérica, solo menciona, no responde
+    var botReply = {
+      in_reply_to_status_id: tweet.id,
+      status: `Hola @${tweet.user.screen_name} .`
+    }
 
-    var botReply = {in_reply_to_status_id: tweet.id, status: "Hola @" + tweet.user.screen_name + " ."} //respuesta genérica, solo menciona, no responde
-
-    //publica la respuesta
-    Twitter.post('statuses/update', botReply,  function(error, tweetReply, response){
-
+    // publica la respuesta
+    bot.post('statuses/update', botReply,  function(err, tweetReply){
+      if (err) {
+        console.error(err)
+        return err
+      }
       // muestra la respuesta que dimos
-      console.log(tweetReply.text);
-    });
-
-  });
-
-});
+      tweetLogger.out(tweetReply.text)
+    })
+  })
+})
